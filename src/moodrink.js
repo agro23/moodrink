65adf156b981c77aa0a625176480a33f91ecd3b1
Andy Grossberg and Andrew Peterson <andy@>
2018-03-22 14:55:37 -0700

    add API key to .ENV file.

--- src/moodrink.js
+++ src/moodrink.js
@@ -1,11 +1,13 @@
 import $ from 'jquery';
 // import fetchResults from "../src/main.js";
+// normally we wouldn't save this in the file
+// API_KEY = https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka
 
 function getDrink(fetchResults, badResults){
 
   let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka`;
+      let url = process.env.API_KEY;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
